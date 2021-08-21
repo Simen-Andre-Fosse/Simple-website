bfe2d7902ee9504481adb88b7c7b37599212233d
Simen Andre Gravdal <simen.gravdal@hotmail.com>
2021-08-21 13:49:16 +0200

    Changes to eventlistener

--- script/main.js
+++ script/main.js
@@ -10,18 +10,17 @@ function print(){
 fetch('http://skbank.azurewebsites.net/api/transaksjon').then(response => response.json())
 .then(data=> console.log(data));
 
-fetch('http://skbank.azurewebsites.net/api/transaksjon').then(response => response.json())
-.then(data=> transaksjoner = data);
-
-
 
 
 
 document.addEventListener('DOMContentLoaded', function() {
     fetch('http://skbank.azurewebsites.net/api/transaksjon').then(response => response.json())
-.then(data=> transaksjoner = data);
-    var transData = JSON.stringify(transData);
-    document.getElementById("demo").innerHTML = transData.kontonavn; 
+.then(data=> {
+    document.getElementById("demo").innerHTML = "Kontonavn: " + data.kontonavn;
+    document.getElementById("demo2").innerHTML = "Saldo: " + data.saldo;
+});
+    
+    
 
   }, false);
 
