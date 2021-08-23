a016c2c1872f45718d376a5552ba0b322dda4dd7
Simen Andre Gravdal <simen.gravdal@hotmail.com>
2021-08-23 11:11:17 +0200

    Update..

--- script/task3_script.js
+++ script/task3_script.js
@@ -1,5 +1,5 @@
-var kategoriData = {};
-
+var kategoriData = [];
+var trans_data = {};
 
 fetch('http://skbank.azurewebsites.net/api/kategori')
 .then(response => response.json())
@@ -10,4 +10,41 @@ fetch('http://skbank.azurewebsites.net/api/kategori')
     
 });
 
+fetch('http://skbank.azurewebsites.net/api/transaksjon')
+.then(response => response.json())
+.then(data=> {
+
+    trans_data = data;
+    
+});
+
+function getCategoryIDs(arr){
+    var liste = [];
+    for(kat of kategoriData){
+        liste.push(kat.kategoriID);
+    }
+
+    return liste;
+}
+
+
+function matchCat(obj1, obj2){
+    if(obj1.kategoriID === obj2.kategoriID){
+        return true; 
+    } else {
+        return false; 
+    }
+
+}
+
+function update_data_object(){
+    var new_data_object = {};
+    var old_data = trans_data;
+
+    var el = getElement("categories");
+    var value = el.options[el.selectedIndex].value;
+
+    return value; 
+
+}
 
