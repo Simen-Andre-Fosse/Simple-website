b1621f5fd61579049175ca53088f470dd6ccba54
Simen Andre Gravdal <simen.gravdal@hotmail.com>
2021-08-23 19:10:39 +0200

    update
    Skriver kommentarer til koden..

--- script/main.js
+++ script/main.js
@@ -1,12 +1,17 @@
+/**
+ * Første script som hovedsaklig kjører hovedsaklig på index vevsiden for oppgave 1. 
+ * Her hentes transaksjonsdata fra API. 
+ * 
+ * Bruker setText funksjon som hentes fra function.js scriptet for å sette verdier til kontonavn og saldo. 
+ */
 var dataObject = {};
-
 
 fetch('http://skbank.azurewebsites.net/api/transaksjon')
 .then(response => response.json())
 .then(data=> {
 
-    setText("kntnavn", ("Kontonavn: " + data.kontonavn + "."));
-    setText("saldo", ("Saldo: " + data.saldo + "Kr."));
+    setText("kntnavn", ("Kontonavn: " + data.kontonavn));
+    setText("saldo", ("Saldo: " + data.saldo + "Kr"));
     dataObject = data;
     
     
